5b292849e6ca25124d52f2f3fe131c05395dbaa4
허제인 <codingjamee@gmail.com>
2024-07-19 00:23:13 +0900

    util함수 추가

---
+++ src/util/utils.ts
@@ -0,0 +1,15 @@
+export const getNextNumber = ({ prevNumber }: { prevNumber: number }) => {
+  if (typeof prevNumber !== "number")
+    return console.log("숫자만 사용할 수 있습니다.");
+  return prevNumber + 1;
+};
+
+export const combineKey = ({
+  key_one,
+  key_two,
+}: {
+  key_one: string;
+  key_two: string;
+}) => {
+  return `${key_one}${key_two}`;
+};
