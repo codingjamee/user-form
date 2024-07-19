afde71fcd055fa3ebea4fa87f99fe3477f0f1a3b
허제인 <codingjamee@gmail.com>
2024-07-20 00:26:55 +0900

    onBlur일때 userOption.options.인덱스 의 값으로 설정

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
+import { OptionProps } from "../types/type";
 
-const Option = ({ optionClass, optionNum, onClickDelete }) => {
+const Option = ({
+  optionClass,
+  setUserOption,
+  index,
+  onClickDelete,
+}: OptionProps) => {
   const [optionDesc, setOptionDesc] = useState("");
 
   return (
-    <section className="options">
-      <div className={optionClass}>{optionNum}</div>
+    <section
+      className="options"
+      onBlur={() =>
+        setUserOption((prev) => ({
+          ...prev,
+          options: { ...prev.options, [`userOption${index + 1}`]: optionDesc },
+        }))
+      }
+    >
+      <div className={optionClass}>{optionClass === "number" && index + 1}</div>
       <input
         type="text"
-        placeholder="옵션 1"
+        placeholder={`옵션 ${index + 1}`}
         onChange={(e) => setOptionDesc(e.target.value)}
       />
-      <div onClick={onClickDelete}>x</div>
+      <div onClick={() => onClickDelete(index)}>x</div>
     </section>
   );
 };
