ac31be0aba1010244b839b29b68df331673dac56
허제인 <codingjamee@gmail.com>
2024-07-28 20:30:46 +0900

    useAdminQueries추가 FetchApi에서 만든 인스턴스 사용해보기

---
+++ src/hooks/useAdminQueries.ts
@@ -0,0 +1,18 @@
+import { AdminGetProps, AdminPostProps } from "../types/type";
+import { adminApi } from "./FetchApi";
+
+const useAdminQueries = () => {
+  const postFormApi = ({ body, config }: AdminPostProps) => {
+    return adminApi.post({ url: "/forms", body, config });
+  };
+  const getResponseApi = (params: AdminGetProps) => {
+    return adminApi.get({ url: "/responses", config: { params } });
+  };
+
+  return {
+    postFormApi,
+    getResponseApi,
+  };
+};
+
+export default useAdminQueries;
