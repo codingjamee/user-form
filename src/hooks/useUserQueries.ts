2f0ecf810546e5755e5b97fb54972e5053e0f7d0
허제인 <codingjamee@gmail.com>
2024-07-28 23:10:26 +0900

    useUserQueries 설정

---
+++ src/hooks/useUserQueries.ts
@@ -0,0 +1,18 @@
+import { GetProps, postResponseApiProps } from "../types/type";
+import { userApi } from "./FetchApi";
+
+const useUserQueries = () => {
+  const postResponseApi = ({ body, config }: postResponseApiProps) => {
+    return userApi.post({ url: "/responses", body, config });
+  };
+  const getFormApi = (params: GetProps) => {
+    return userApi.get({ url: "/forms", config: { ...params } });
+  };
+
+  return {
+    postResponseApi,
+    getFormApi,
+  };
+};
+
+export default useUserQueries;
