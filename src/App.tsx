a90cc260aa660c769c9da5dce6fd461827b1436c
허제인 <codingjamee@gmail.com>
2024-07-28 20:28:57 +0900

    router 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import Forms from "./components/admin/Forms.tsx";
 import UserForms from "./components/user/UserForms.tsx";
-import Home from "./components/home/Home.tsx";
+import Layout from "./components/layout/Layout.tsx";
+import AdminLayout from "./components/layout/AdminLayout.tsx";
+import NewFormBtn from "./components/admin/NewFormBtn.tsx";
+import Responses from "./components/admin/Responses.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route element={<Home />}>
-        <Route path="/admin/forms" element={<Forms />} />
+      <Route path="/" element={<Layout />} />
+      <Route element={<Layout />}>
+        <Route element={<AdminLayout />}>
+          <Route path="/admin/forms/" element={<NewFormBtn />} />
+          <Route path="/admin/forms/:formId/edit" element={<Forms />} />
+          <Route
+            path="/admin/forms/:formId/responses"
+            element={<Responses />}
+          />
+        </Route>
         <Route path="/user/forms" element={<UserForms />} />
         <Route path="/user/forms/:formId" element={<UserForms />} />
       </Route>
