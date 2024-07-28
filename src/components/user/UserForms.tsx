c70425c305181c996a867670433f75354e58cf04
허제인 <codingjamee@gmail.com>
2024-07-28 21:20:43 +0900

    GetForm삭제: UI만을 위한 컴포넌트이므로 삭제

--- src/components/user/UserForms.tsx
+++ src/components/user/UserForms.tsx
@@ -1,29 +1,72 @@
-import { useEffect, useState } from "react";
-import GetForm from "./GetForm";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import useUserForm from "./hooks/useUserForm";
+import { createDefaultUserFormObj } from "../../util/utils";
+import { FormAnswer } from "../../types/type";
+import FormOptions from "./FormOptions";
 
 const UserForms = () => {
-  const [data, setData] = useState();
-
   const { formId } = useParams();
+  const { data } = useUserForm({ formId });
+  const [groupOption, setGroupOption] = useState<FormAnswer | undefined>({
+    questionId: formId,
+    responses: createDefaultUserFormObj().responses,
+  });
 
-  const getFormData = async () => {
-    try {
-      const result = await fetch(`/api/forms/${formId}`, {
-        method: "GET",
-      });
-      const resultText = await result.json();
-      setData(resultText);
-    } catch (err) {
-      console.log(err);
-    }
+  const onSubmitForm = () => {
+    //submit할 때 groupOption을 fetch로 보내기
   };
+  console.log(data?.forms);
 
-  useEffect(() => {
-    getFormData();
-  }, []);
+  return (
+    <form className="app" onSubmit={onSubmitForm}>
+      <div className="form">
+        <section className="title-section title">
+          <div className="edge"></div>
+          <div className="zone">
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div>제목:</div>
+              <input
+                type="text"
+                placeholder="설문지 제목"
+                defaultValue={data?.formTitle?.title}
+              />
+            </div>
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div>설문지 설명:</div>
+              <input
+                type="text"
+                className="desc"
+                placeholder="설문지 설명"
+                defaultValue={data?.formTitle?.desc}
+              />
+            </div>
+          </div>
+        </section>
 
-  return <GetForm data={data} />;
+        <article
+          className="user-section get"
+          style={{ display: "flex", gap: "30px" }}
+        >
+          {data &&
+            data?.forms?.map((form) => (
+              <FormOptions
+                key={form?.id}
+                form={form}
+                setGroupOption={setGroupOption}
+              />
+            ))}
+        </article>
+      </div>
+      <button
+        className="btn"
+        style={{ width: "640px" }}
+        onClick={() => console.log("제출버튼 클릭")}
+      >
+        제출
+      </button>
+    </form>
+  );
 };
 
 export default UserForms;
