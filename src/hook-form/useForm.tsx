3d202b6bc91037a37f93d7827ed52cf85d983927
허제인 <codingjamee@gmail.com>
2024-09-11 22:28:30 +0900

    useForm 단일책임의 원칙 지켜서 수정 작어붖ㅇ

--- src/hook-form/useForm.tsx
+++ src/hook-form/useForm.tsx
@@ -3,19 +3,79 @@ import { FormContext } from "./formContext";
 import { ulid } from "ulid";
 import cloneDeep from "lodash.clonedeep";
 
+const splitKeyWithDot = ({ key }: { key: string }) => {
+  return key.split(".");
+};
+
+const getNestedKeyExceptLast = ({ targetKeys, newValue }) => {
+  const targetKeyArr = splitKeyWithDot({ key: targetKeys });
+  const lastKey = targetKeyArr[targetKeyArr.length - 1];
+  return targetKeyArr.slice(0, -1).reduce((acc, cur) => {
+    if (!acc[cur] || typeof acc[cur] !== "object") {
+      acc[cur] = {};
+    }
+    return { nestedKeyExceptLast: acc[cur], lastKey };
+  }, newValue);
+};
+
+const setNestedValue = ({ setData, targetKeys, value }) => {
+  setData((prev) => {
+    const newValue = { ...prev };
+    const { nestedKeyExceptLast, lastKey } = getNestedKeyExceptLast({
+      targetKeys,
+      newValue,
+    });
+    return (nestedKeyExceptLast[lastKey] = value);
+  });
+};
+
+const setValue = ({ setData, targetKey, value }) => {
+  setData((prev) => ({
+    ...prev,
+    [targetKey]: value,
+  }));
+};
+
+const getNestedValue = ({ data, targetKeys }) => {
+  const targetKeyArr = splitKeyWithDot({ key: targetKeys });
+
+  return targetKeyArr.reduce((acc, cur) => {
+    if (!isNaN(Number(cur))) return acc[Number(cur)];
+    return acc[cur];
+  }, data);
+};
+
+const getValue = ({ data, targetKey }) => {
+  return data[targetKey];
+};
+
+const determineNestedArray = ({ key }: { key: string }) => {
+  //key를 그대로 줄 것인가? splited된 것으로 줄것인가?
+  // const isArray = Array.isArray(target);
+  // return isArray ? "array" : "string";
+  return true;
+};
+
+const validateFormKey = ({ key }: { key: string }) => {
+  /**
+   *  반드시 string으로 시작해서 string으로 끝나야함.
+   *  string 다음에 string이 존재하지 않음.
+   *  number 다음에 number도 존재하지 않음.
+   */
+
+  return {};
+};
+
 const findTargetValue = ({
   data,
   targetKeys,
 }: {
   data: string;
-  targetKeys: string[] | string;
+  targetKeys?: string[] | string;
 }) => {
-  if (Array.isArray(targetKeys))
-    return targetKeys.reduce((acc, cur) => {
-      if (!isNaN(Number(cur))) return acc[Number(cur)];
-      return acc[cur];
-    }, data);
-
+  determineNestedArray(targetKeys);
+  if (Array.isArray(targetKeys)) return getNestedValue({ data, targetKeys });
+  //만약 array가 아니라면
   return data[targetKeys];
 };
 
@@ -27,52 +87,63 @@ const useForm = ({ defaultValues }: { defaultValues?: {} }) => {
     setData(defaultValues);
   }, []);
 
-  const register = (name: string) => {
+  /**
+   *  register함수 설명 :
+   *  data를 등록하는 함수,
+   *  return한 함수 onChange와 name, value는
+   *  전달한 name에 대한 함수
+   *
+   *  매개변수로 받은 name (type string)을 key로 활용한다.
+   *  nested되어있는 경우 key가 숫자와 string이 .을 기점으로 번갈아가며 존재
+   *  규칙 :
+   *  반드시 string으로 시작해서 string으로 끝나야함.
+   *  string 다음에 string이 존재하지 않음.
+   *  number 다음에 number도 존재하지 않음.
+   */
+
+  /**
+   *
+   * @param name
+   * @returns onChange, name, value
+   */
+  const register = (
+    name: string
+  ): {
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    name?: string | string[];
+    value?: any;
+    validationResult: { isValid: boolean; error: string };
+  } => {
     // 해당 이름으로 data를 등록
+
+    const isValidKey = validateFormKey({ key: name });
+
+    if (!isValidKey)
+      return { validationResult: { isValid: false, error: "not valid key" } };
 
     const keys = name.split("."); //usefieldArray사용시
     const popKeys = [...keys]; //마지막 key를 pop하기 위해 copy
-    const poppedKey = popKeys.pop(); //마지막 key를 pop
+    const poppedKey =
+      Array.isArray(popKeys) && popKeys.length > 1 ? popKeys.pop() : undefined; //마지막 key를 pop
     const targetKey = findTargetValue({ data, targetKeys: popKeys });
 
-    const registerToData = () => {
-      const isThereFormArray = keys.length > 1;
-
-      if (isThereFormArray) {
-        const mutatedData = { ...data };
-
-        for (let i = 0; i < keys.length; i++) {
-          //등록하고자 하는 key value가 존재하는 경우 return
-          const isTherePrevVal = mutatedData[keys[i]];
-          if (isTherePrevVal) return;
-
-          //다음 요소가 숫자일때 해당 키의 값은 array
-          const nextValue = parseInt(keys[i + 1]) + 1;
-          const valueIsArray = !isNaN(nextValue);
-
-          const arrayCount = nextValue;
-
-          if (valueIsArray) {
-            mutatedData[keys[i]] = [];
-            for (let i = 0; i < arrayCount; i++) {
-              mutatedData[keys[i]].push({ id: ulid() });
-            }
-            return; //{ a: []}
-          }
-          return (mutatedData[keys[i]] = { id: ulid() });
-        }
-
-        setData(mutatedData);
-      }
-    };
-
-    registerToData();
-
+    const isNestedArray = determineNestedArray({ key: name });
     return {
       onChange: (e: ChangeEvent<HTMLInputElement>) => {
-        targetKey[poppedKey] = e.target.value;
+        if (isNestedArray) {
+          return setNestedValue({
+            setData,
+            targetKeys: name,
+            value: e.target.value,
+          }); //nestedValue를 등록
+        }
+        return setValue({ targetKey: name, value: e.target.value, setData });
       },
       name: poppedKey,
+      value: isNestedArray
+        ? getNestedValue({ data, targetKeys: name })
+        : getValue({ data, targetKey }),
+      validationResult: { isValid: true, error: "" },
     };
   };
 
